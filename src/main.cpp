c954884abe154f89630297457bfffdd48c7f4a98
Konzept59 <131675403+Konzept59@users.noreply.github.com>
2023-04-25 14:10:57 +0200

    Update main.cpp
    Adding analog Outputs
    -) Pin ~9, ~10, ~11
    -) Pressure from 0hPa(0V) to 1090hPa(5V)

--- src/main.cpp
+++ src/main.cpp
@@ -16,13 +16,16 @@ DHT dht(DHT22_Pin, DHTTYPE);
 // declatioon of humidit and temperataur variables
 float dhtHumidity, dhtTemperature;
 
-// I2C Interface
-Adafruit_BMP280 bmp; 
+
+Adafruit_BMP280 bmp; // use I2C interface
+Adafruit_Sensor *bmp_temp = bmp.getTemperatureSensor();
+Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
 
 void setup() {
   // Serial begin baud 9600
   Serial.begin(9600);
 
+  
   // check if BMP280 works!
   Serial.println(F("BMP280 test"));
   if  (!bmp.begin()) {
@@ -30,13 +33,14 @@ void setup() {
     while (1);
   }
 
-  /* Default settings from datasheet.  */
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500);  /* Standby time. */
+  // Default settings from datasheet.  
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     // Operating Mode. 
+                  Adafruit_BMP280::SAMPLING_X2,     // Temp. oversampling 
+                  Adafruit_BMP280::SAMPLING_X16,    // Pressure oversampling 
+                  Adafruit_BMP280::FILTER_X16,      // Filtering. 
+                  Adafruit_BMP280::STANDBY_MS_500); //  Standby time. 
   
+
   // starting dht Sensor
   dht.begin();
 
@@ -52,6 +56,8 @@ void loop() {
     char bmpTemperature[16];
     char bmpPressure[16];
     char bmpAltitude[16];
+
+    float pressure = bmp.readPressure()/100;
 
 
     // Reading Temperature, Pressure and Altitude 
@@ -71,11 +77,8 @@ void loop() {
 
     // Printing Altitude from bmp280 Sensor
     Serial.print(F("Approx altitude = "));
-    Serial.print(bmp.readAltitude(1019.66));  //The "1019.66" is the pressure(hPa) at sea level in day in your region
+    Serial.print(bmp.readAltitude(1012));  //The "1019.66" is the pressure(hPa) at sea level in day in your region
     Serial.println("  m");                    //If you don't know it, modify it until you get your current  altitude
-
-    // printing one empty line 
-    Serial.println("-------------------------------");
 
     // reading Humidity and Temperature from DHT22 Sensor
     dhtHumidity = dht.readHumidity();
@@ -91,14 +94,22 @@ void loop() {
     Serial.print(dhtTemperature);
     Serial.println(" °C");
 
+    // printing one empty line 
+    Serial.println("-------------------------------");
+
+    float analogTemp = (256/50)*dhtTemperature;
+    float analogHum = (256/100) * dhtHumidity;
+    double analogPressure = 0.2348623853211009 * pressure;
+
     // Sending Pressure, Temeperature and Humidity Data to Hioki LR8450
-    analogWrite(A0, bmp.readAltitude());
-    analogWrite(A1, bmp.readTemperature());
-    analogWrite(A3, dhtHumidity);
+    analogWrite(9, analogPressure);
+    analogWrite(10, analogTemp);
+    analogWrite(11, analogHum);
+
+    
 
     // Delay for 2 seconds
     delay(2000);
   
   
 }
-
